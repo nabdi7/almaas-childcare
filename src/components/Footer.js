b322234ac28cc53fda6fc2cc7931f3108aae1ab2
nabdi7 <36966910+nabdi7@users.noreply.github.com>
2023-11-28 23:48:45 -0800

    home changes

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,13 +1,26 @@
 import './Footer.css';
+import logo from "../images/almaas-transparent.png"
 
 
-const Footer = () =>{
+const Footer = () => {
     return (
-        <div className="footer">
-            <div className="footer-container">
-                <h1> Foooter </h1>
+        <>
+        <footer className='footer'>
+            <div className='logo-div'>
+                <img src={logo} width='200px' height='auto' max-width='100%' max-height='100%' alt='Logo' />
             </div>
-        </div>
+
+            <div className='footer-container'>
+                {/* <h1 className='footer-title'> Almaas Family Home Childcare</h1> */}
+                <p className='footer-description'><a href='https://www.google.com/maps/place/Almaas+Family+Childcare/@47.3690973,-122.1280788,15z/data=!4m2!3m1!1s0x0:0xf4355668dfba3d39?sa=X&ved=2ahUKEwj9s5P5puiCAxWVODQIHfqNAUMQ_BJ6BAhIEAA'>25921 161st CT SE Covington, WA 98042 United States </a></p>
+                <p className='footer-description'> <a href='tel:+12068833462'><b>(206)934-9581</b></a></p>
+            </div>
+            <div className="copyright">
+                <p>Made by <a href="https://qualitydesigns.site/" target="_blank" rel="noopener noreferrer" style={{ color: 'black', fontWeight: 'bold'}}>Quality Designs</a> || ©{new Date().getFullYear()} Almaas Childcare</p>
+            </div>
+        </footer>
+    
+        </>
     );
 }
 
--- src/components/Home.js
+++ src/components/Home.js
@@ -7,7 +7,7 @@ import image3 from '../images/image3.jpeg';
 import image4 from '../images/kid4.jpeg';
 import image5 from '../images/kid6.jpeg';
 import image6 from '../images/kid5.jpeg';
-import aboutImage from '../images/background.jpg';
+import aboutImage from '../images/aboutImg1.jpeg';
 import toddler from '../images/toddler.jpg';
 import preschool from '../images/preshool.jpg';
 import schoolAge from '../images/school.jpg';
@@ -35,36 +35,46 @@ const Home = () => {
         </div>
 
         {/* about the daycare  */}
-        <section className='about about-container'>
-            <div className='left'>
-                <h2 class="about-title">Welcome to Takoma Park Child Development Center</h2>
-                <h3>Love for Learning. Respect for Others. </h3>
-                <p>TPCDC's mission is to provide children with a safe and nurturing environment where a positive sense of self, feelings of trust and security, and a love for learning and respect for others will develop. The Center is dedicated to promoting a harmonious, diverse community, and to supporting families by providing consistent, convenient, affordable, and high quality child care.</p>
+        <div class="aboutSection">
+            <div class="leftSide"> 
+                <img src={aboutImage} alt='program' />
+            </div>
+            
+            <div class="rightSide">
+                <h2>Welcome to Almaas Family Childcare</h2>
+                
+                <p>We offer a high-quality learning center located in Rockwall, 
+                    Texas - a truly new, innovative and affordable concept in 
+                    childcare and preschool education. Our full-time childcare 
+                    center offers the finest facilities available for children 
+                    ages 6 weeks to school-aged. Our preschool program is designed 
+                    for 3 to 5 year olds to prepare them for their all-important 
+                    school years ahead. We offer an extensive school-age program 
+                    for before and after school for ages 5 to 12 years old with 
+                    age appropriate S.T.E.A.M. MakerSpace activities.
+                </p>
+
                 <br/>
                 <br/>
                 <h3>Highlights/Quick Facts:</h3> 
-                <ul>
+                <ul className='bodycontent'>
                     <li> Year-round, full time preschool program for ages 18 months-5 years</li>
                     <li>Before &amp; after care and summer camp for grades K-6</li>
-                    <li>Open Monday-Friday, 7 AM-6 PM</li>
+                    <li>Open Monday-Friday, 5 AM-9 PM</li>
                     <li>Developmentally appropriate play-based curriculum</li>
                     <li>Qualified, caring &amp; experienced Staff</li>
                     <li>Nutritious meals &amp; snacks </li>
                     <li>Located two blocks from the Takoma Metro/Downtown Takoma Park</li>
                 </ul>
 
-                <p>Please feel free to <a href="contact-us.html" previewlistener="true">contact us</a> to setup a time when you can come tour our facility and meet our staff. We look forward to meeting you and your children. </p>
-               <br />
-            </div>
-            
-            <div className='right'>
-                <img src={aboutImage} alt='program' />
-            </div>
-
-
-        </section>
-
-
+                <br/>
+                <br/>
+                <p>Please feel free to contact us at <b> (206)934-9581 </b> to setup a time when you can come tour our facility and meet our staff. We look forward to meeting you and your children. </p>
+                
+                <br />
+            </div>	
+        
+        </div>
 
         {/* gallary  */}
         <h1 className='gallery-title'> PHOTO GALLERY</h1>
@@ -127,23 +137,45 @@ const Home = () => {
 
         {/* testimonials */}
         <section className='testimonials'>
-
+        
 
         </section>
 
 
         {/* contact us  */}
-        <section className='contact'>
+        <section className='contactSection'>
+            <div class="leftCblock">
+                <h1>Our location</h1>
+                <iframe
+                    src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d10808.783105947387!2d-122.1280788!3d47.3690973!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x54905f1967c7aaa7%3A0xf4355668dfba3d39!2sAlmaas%20Family%20Childcare!5e0!3m2!1sen!2sus!4v1701227534068!5m2!1sen!2sus"
+                    width="600"
+                    height="450"
+                    style={{ border: 0 }}
+                    allowFullScreen=""
+                    loading="lazy"
+                    referrerPolicy="no-referrer-when-downgrade"
+                ></iframe>
+            </div>
 
+            <div class="rightCBlock">
+                <div class="desc"> 
+                    <h1>Get In Touch</h1>
+                    <form>
+                        <div>
+                            <input type="text" id="name" name="name" placeholder="Name" />
+                        </div>
+                        <div>
+                            <input type="email" id="email" name="email" placeholder="Email" />
+                        </div>
+                        <div>
+                            <textarea id="message" name="message" placeholder="Message"></textarea>
+                        </div>
+                        <button className='submit'>Send</button>
+                    </form>
+                </div>
+            </div>
 
         </section>
-
-        {/* maps  */}
-        <section className='maps'>
-
-
-        </section>
-
       
     </>
     );
