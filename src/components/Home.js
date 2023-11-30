777fd25f0a4b86b5a4ead7c30443f385923880b2
nabdi7 <36966910+nabdi7@users.noreply.github.com>
2023-11-30 00:18:13 -0800

    programs

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link, Element } from 'react-scroll';
 import './Home.css';
 import Gallery from './Gallery';
 import image1 from '../images/image1.jpeg';
@@ -35,47 +36,50 @@ const Home = () => {
         </div>
 
         {/* about the daycare  */}
-        <div class="aboutSection">
-            <div class="leftSide"> 
-                <img src={aboutImage} alt='program' />
-            </div>
+        <Link to='aboutSection' smooth={true} duration={1000}></Link>
+        <Element name='aboutSection'>
+            <div class="aboutSection">
+                <div class="leftSide"> 
+                    <img src={aboutImage} alt='program' />
+                </div>
+                
+                <div class="rightSide">
+                    <h2>Welcome to Almaas Family Childcare</h2>
+                    
+                    <p>We offer a high-quality learning center located in Covington, 
+                        WA - a truly new, innovative and affordable concept in 
+                        childcare. Our full-time childcare offers the finest facilities available for children 
+                        ages new born to school-aged. Almaas Family Childcare 
+                        provides a high-quality and professional caregiver's home. 
+                        It's our aim to provide fun and caring. Let us know what 
+                        you need for your child because you won't find better 
+                        daycare in King County. Contact us at <b> (206)934-9581 </b> 
+                        for more information
+                    </p>
+
+                    <br/>
+                    <br/>
+                    <h3>Highlights/Quick Facts:</h3> 
+                    <ul className='bodycontent'>
+                        <li>Developmentally appropriate play-based curriculum</li>
+                        <li>Qualified, caring &amp; experienced Staff</li>
+                        <li> Year-round, full time program</li>
+                        <li>Open Everyday, 5 AM-9 PM</li>
+                        <li>Before &amp; after school care </li>
+                        <li>Nutritious meals &amp; snacks </li>
+                    </ul>
+
+                    <br/>
+                    <br/>
+                    <p>Please feel free to contact us at <b> (206)934-9581 </b> to setup a 
+                    time when you can come tour our facility and meet. We look 
+                    forward to meeting you and your children. </p>
+                    
+                    <br />
+                </div>	
             
-            <div class="rightSide">
-                <h2>Welcome to Almaas Family Childcare</h2>
-                
-                <p>We offer a high-quality learning center located in Covington, 
-                    WA - a truly new, innovative and affordable concept in 
-                    childcare. Our full-time childcare offers the finest facilities available for children 
-                    ages new born to school-aged. Almaas Family Childcare 
-                    provides a high-quality and professional caregiver's home. 
-                    It's our aim to provide fun and caring. Let us know what 
-                    you need for your child because you won't find better 
-                    daycare in King County. Contact us at <b> (206)934-9581 </b> 
-                    for more information
-                </p>
-
-                <br/>
-                <br/>
-                <h3>Highlights/Quick Facts:</h3> 
-                <ul className='bodycontent'>
-                    <li>Developmentally appropriate play-based curriculum</li>
-                    <li>Qualified, caring &amp; experienced Staff</li>
-                    <li> Year-round, full time program</li>
-                    <li>Open Everyday, 5 AM-9 PM</li>
-                    <li>Before &amp; after school care </li>
-                    <li>Nutritious meals &amp; snacks </li>
-                </ul>
-
-                <br/>
-                <br/>
-                <p>Please feel free to contact us at <b> (206)934-9581 </b> to setup a 
-                time when you can come tour our facility and meet. We look 
-                forward to meeting you and your children. </p>
-                
-                <br />
-            </div>	
-        
-        </div>
+            </div>
+        </Element>
 
         {/* gallary  */}
         <h1 className='gallery-title'> PHOTO GALLERY</h1>
@@ -85,57 +89,59 @@ const Home = () => {
 
 
         {/* programs  */}
-        <section className='programs programBox'>
-            <h2> Our Programs</h2>
-            <div className='programs-container'>
-                <a href='#' className='program' previewlistener="true">
-                    <div className='program-image'>
-                        <img src={toddler} alt='program' />
-                    </div>
-                    <div className='program-type'>
-                        <h3>Infant Program</h3>
-                        <p class="ageRange">18 mo to 2 years</p>
-                        <p>Our infant program provides a nurturing environment for children aged 18 months to 2 years, fostering early development and exploration.</p>
+        <Link to='servicesSection' smooth={true} duration={1000}></Link>
+        <Element name='servicesSection'>
+            <section className='programs programBox'>
+                <h2> Our Programs</h2>
+                <div className='programs-container'>
+                    <a href='#' className='program' previewlistener="true">
+                        <div className='program-image'>
+                            <img src={toddler} alt='program' />
+                        </div>
+                        <div className='program-type'>
+                            <h3>Infant Program</h3>
+                            <p class="ageRange">18 mo to 2 years</p>
+                            <p>Our infant program provides a nurturing environment for children aged 18 months to 2 years, fostering early development and exploration.</p>
 
-                    </div>
-                </a>
+                        </div>
+                    </a>
 
-                <a href='#' className='program' previewlistener="true">
-                    <div className='program-image'>
-                        <img src={[preschool]} alt='program' />
-                    </div>
-                    <div className='program-type'>
-                        <h3>Preschool</h3>
-                        <p class="ageRange">2 to 5 years</p>
-                        <p>Our preschool program focuses on foundational skills and social interaction for children aged 2 to 5 years, preparing them for a smooth transition to formal education.</p>
-                    </div>
-                </a>
+                    <a href='#' className='program' previewlistener="true">
+                        <div className='program-image'>
+                            <img src={[preschool]} alt='program' />
+                        </div>
+                        <div className='program-type'>
+                            <h3>Preschool</h3>
+                            <p class="ageRange">2 to 5 years</p>
+                            <p>Our preschool program focuses on foundational skills and social interaction for children aged 2 to 5 years, preparing them for a smooth transition to formal education.</p>
+                        </div>
+                    </a>
 
-                <a href='#' className='program' previewlistener="true">
-                    <div className='program-image'>
-                        <img src={schoolAge} alt='program' />
-                    </div>
-                    <div className='program-type'>
-                        <h3>School Age</h3>
-                        <p class="ageRange">5 to 12 years</p>
-                        <p>Our school-age program offers a stimulating environment for children aged 5 to 12 years, combining education and recreation to support their ongoing development.</p>
-                    </div>
-                </a>
+                    <a href='#' className='program' previewlistener="true">
+                        <div className='program-image'>
+                            <img src={schoolAge} alt='program' />
+                        </div>
+                        <div className='program-type'>
+                            <h3>School Age</h3>
+                            <p class="ageRange">5 to 12 years</p>
+                            <p>Our school-age program offers a stimulating environment for children aged 5 to 12 years, combining education and recreation to support their ongoing development.</p>
+                        </div>
+                    </a>
 
-                <a href='#' className='program' previewlistener="true">
-                    <div className='program-image'>
-                        <img src={image1} alt='program' />
-                    </div>
-                    <div className='program-type'>
-                        <h3>Other Programs</h3>
-                        <p class="ageRange">5 to 12 years</p>
-                        <p>Explore our diverse range of programs tailored to different age groups and interests, ensuring a holistic and enriching experience for all participants.</p>
-                    </div>
-                </a>
-            </div>
+                    <a href='#' className='program' previewlistener="true">
+                        <div className='program-image'>
+                            <img src={image1} alt='program' />
+                        </div>
+                        <div className='program-type'>
+                            <h3>Other Programs</h3>
+                            <p class="ageRange">5 to 12 years</p>
+                            <p>Explore our diverse range of programs tailored to different age groups and interests, ensuring a holistic and enriching experience for all participants.</p>
+                        </div>
+                    </a>
+                </div>
 
-        </section>
-
+            </section>
+        </Element>
 
         {/* testimonials */}
         <section className='testimonials'>
@@ -145,39 +151,42 @@ const Home = () => {
 
 
         {/* contact us  */}
-        <section className='contactSection'>
-            <div class="leftCblock">
-                <h1>Our location</h1>
-                <iframe
-                    src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d10808.783105947387!2d-122.1280788!3d47.3690973!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x54905f1967c7aaa7%3A0xf4355668dfba3d39!2sAlmaas%20Family%20Childcare!5e0!3m2!1sen!2sus!4v1701227534068!5m2!1sen!2sus"
-                    width="600"
-                    height="450"
-                    style={{ border: 0 }}
-                    allowFullScreen=""
-                    loading="lazy"
-                    referrerPolicy="no-referrer-when-downgrade"
-                ></iframe>
-            </div>
-
-            <div class="rightCBlock">
-                <div class="desc"> 
-                    <h1>Get In Touch</h1>
-                    <form>
-                        <div>
-                            <input type="text" id="name" name="name" placeholder="Name" />
-                        </div>
-                        <div>
-                            <input type="email" id="email" name="email" placeholder="Email" />
-                        </div>
-                        <div>
-                            <textarea id="message" name="message" placeholder="Message"></textarea>
-                        </div>
-                        <button className='submit'>Send</button>
-                    </form>
+        <Link to='contactSection' smooth={true} duration={1000}></Link>
+        <Element name='contactSection'>
+            <section className='contactSection'>
+                <div class="leftCblock">
+                    <h1>Our location</h1>
+                    <iframe
+                        src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d10808.783105947387!2d-122.1280788!3d47.3690973!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x54905f1967c7aaa7%3A0xf4355668dfba3d39!2sAlmaas%20Family%20Childcare!5e0!3m2!1sen!2sus!4v1701227534068!5m2!1sen!2sus"
+                        width="600"
+                        height="450"
+                        style={{ border: 0 }}
+                        allowFullScreen=""
+                        loading="lazy"
+                        referrerPolicy="no-referrer-when-downgrade"
+                    ></iframe>
                 </div>
-            </div>
 
-        </section>
+                <div class="rightCBlock">
+                    <div class="desc"> 
+                        <h1>Get In Touch</h1>
+                        <form>
+                            <div>
+                                <input type="text" id="name" name="name" placeholder="Name" />
+                            </div>
+                            <div>
+                                <input type="email" id="email" name="email" placeholder="Email" />
+                            </div>
+                            <div>
+                                <textarea id="message" name="message" placeholder="Message"></textarea>
+                            </div>
+                            <button className='submit'>Send</button>
+                        </form>
+                    </div>
+                </div>
+
+            </section>
+        </Element>
       
     </>
     );
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,36 +1,54 @@
-import React from'react';
+import React from 'react';
+
+import { Link as ScrollLink } from 'react-scroll';
+import logo from '../images/almaas-transparent.png';
 import './Navbar.css';
-import { Link } from 'react-router-dom';
-import logo from "../images/almaas-transparent.png"
-
-
 
 const Navbar = () => {
-    return (
-        <div className="navbar">
-            <div className="container">
-                <div className="navbar-links-left">
-                    <ul>
-                        <li><Link to="/">Home</Link></li>
-                        <li><Link to="/about">About</Link></li>
-                    </ul>
-                </div>
-                <div className="navbar-logo">
-                    <img src={logo} width='190px' height='auto' max-width='100%' max-height='100%' alt='Logo' />
-                </div>
-                <div className="navbar-links-right">
-                    <ul>
-                        <li><Link to="/services">Services</Link></li>
-                        <li>
-                            <Link to="/contact" className='active'>
-                                Contact
-                            </Link>
-                        </li>
-                    </ul>
-                </div>
-            </div>
+  return (
+    <div className='navbar'>
+      <div className='container'>
+        <div className='navbar-links-left'>
+          <ul>
+            <li>
+              <ScrollLink to='homeSection' smooth={true} duration={1000}>
+                Home
+              </ScrollLink>
+            </li>
+            <li>
+              <ScrollLink to='aboutSection' smooth={true} duration={1000}>
+                About
+              </ScrollLink>
+            </li>
+          </ul>
         </div>
-    );
-}
+        <div className='navbar-logo'>
+          <img
+            src={logo}
+            width='190px'
+            height='auto'
+            max-width='100%'
+            max-height='100%'
+            alt='Logo'
+          />
+        </div>
+        <div className='navbar-links-right'>
+          <ul>
+            <li>
+              <ScrollLink to='servicesSection' smooth={true} duration={1000}>
+                Services
+              </ScrollLink>
+            </li>
+            <li>
+              <ScrollLink to='contactSection' smooth={true} duration={1000}>
+                Contact
+              </ScrollLink>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default Navbar;
